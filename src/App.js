aefad8dfa6b638cdcc5f0412d8ead38b3a5f2bb6
Subrata_Roy <subrata.r321@gmail.com>
2021-07-13 16:21:16 -0400

    create necessery components file with functional component, import Browser router, route, switch from react-router-dom, Assign route with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Header from './components/Header'
+import Home from './components/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
+          <Redirect path="/" to="/home" />
+        </Switch>
+      </Router>
     </div>
   );
 }
